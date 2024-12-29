45c99779ceb80080471b9ffab7142ce943dd1af9
Asen Chochev <asen.qkiqt145@gmail.com>
2024-12-29 22:21:34 +0200

    Changed res.statusCode with res.status

--- backend/error/error.js
+++ backend/error/error.js
@@ -1,18 +1,18 @@
-class ErrorHandler extends Error{
-    constructor(message, statusCode){
+class ErrorHandler extends Error {
+    constructor(message, statusCode) {
         super(message);
         this.statusCode = statusCode;
     }
 }
 
 export const errormiddleware = (err, req, res, next) => {
-    err.message = err.message || "Internal server  Error!";
+    err.message = err.message || "Internal server error!";
     err.statusCode = err.statusCode || 500;
 
-    return res.statusCode(err.statusCode).json({
+    return res.status(err.statusCode).json({
         success: false,
         message: err.message,
     });
 };
 
-export default ErrorHandler; 
+export default ErrorHandler;
