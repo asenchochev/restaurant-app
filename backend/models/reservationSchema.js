55c9d9fc5b06c6a513d01c1ca7bcfd15de38691a
Asen Chochev <asen.qkiqt145@gmail.com>
2024-12-29 22:31:39 +0200

    Update "LastName" with "lastName"

--- backend/models/reservationSchema.js
+++ backend/models/reservationSchema.js
@@ -9,7 +9,7 @@ const reservationSchema = new mongoose.Schema({
         minLength: [3, "First name must contain at least 3 characters!"],
         maxLength: [30, "First name cannot exceed 30 characters!"]
     },
-    LastName: {
+    lastName: {
         type: String,
         required: true,
         minLength: [3, "Last name must contain at least 3 characters!"],
